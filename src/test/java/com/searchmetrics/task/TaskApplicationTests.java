6bf1911ba4b207cf7e3a72cc7485e9396385017d
Coditori <massoud.afrashteh@gmail.com>
2021-05-24 16:44:21 +0430

    add first failed test

--- src/test/java/com/searchmetrics/task/TaskApplicationTests.java
+++ src/test/java/com/searchmetrics/task/TaskApplicationTests.java
@@ -1,13 +1,26 @@
 package com.searchmetrics.task;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate restTemplate;
 
+	@Test
+	void whenCallingTheApi_returnBody_success() {
+		ResponseEntity<Object> response = restTemplate.getForEntity("/prices/BTCUSDT",
+				Object.class);
+
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+	}
 }
